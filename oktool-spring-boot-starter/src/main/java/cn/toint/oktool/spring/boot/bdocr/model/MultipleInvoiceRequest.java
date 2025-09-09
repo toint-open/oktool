18d1065f016013fa9447edcd5880ead9a62fc7de
Toint <599818663@qq.com>
2025-09-10 06:35:57 +0800

    增加注释

--- oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/bdocr/model/MultipleInvoiceRequest.java
+++ oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/bdocr/model/MultipleInvoiceRequest.java
@@ -14,7 +14,7 @@ import lombok.EqualsAndHashCode;
 @Data
 public class MultipleInvoiceRequest extends BaseOcrRequest {
     /**
-     * 是否开启验真，默认为 false，即不开启，当为 true 时，返回匹配发票验真接口所需的6要素信息，具体返回信息详见末尾说明
+     * 是否开启验真，默认为 false，即不开启，当为 true 时，只会返回匹配发票验真接口所需的6要素信息, 不会返回原有识别结果.
      */
     @JsonProperty("verify_parameter")
     private boolean verifyParameter;
