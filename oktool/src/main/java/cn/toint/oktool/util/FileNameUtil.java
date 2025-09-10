d6c8932c207b363f1a21b5687d61d5ad4d5b2814
Toint <599818663@qq.com>
2025-09-10 17:57:40 +0800

    add FileNameUtil

---
+++ oktool/src/main/java/cn/toint/oktool/util/FileNameUtil.java
@@ -0,0 +1,31 @@
+package cn.toint.oktool.util;
+
+import cn.hutool.v7.core.text.StrUtil;
+
+/**
+ * @author Toint
+ * @dete 2025/9/10
+ */
+public class FileNameUtil extends cn.hutool.v7.core.io.file.FileNameUtil {
+    /**
+     * 重命名文件主名称(不会修改后缀)
+     *
+     * @param filePath        文件
+     * @param newFileMainName 新的文件主名称(不含后缀)
+     * @return 重命名后的文件名称
+     */
+    public static String renameMain(final String filePath, final String newFileMainName) {
+        String fileName = getName(filePath);
+        if (StrUtil.isBlank(fileName)) {
+            return newFileMainName;
+        }
+
+        // 如果原始文件名称有后缀则保留
+        final String suffix = getSuffix(fileName);
+        if (StrUtil.isBlank(suffix)) {
+            return newFileMainName;
+        } else {
+            return newFileMainName + "." + suffix;
+        }
+    }
+}
---
+++ oktool/src/test/java/cn/toint/oktool/util/FileNameUtilTest.java
@@ -0,0 +1,18 @@
+package cn.toint.oktool.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Toint
+ * @dete 2025/9/10
+ */
+public class FileNameUtilTest {
+
+    @Test
+    void renameMainTest() {
+        Assertions.assertEquals("1.pdf", FileNameUtil.renameMain("a.b.pdf", "1"));
+        Assertions.assertEquals("a.pdf", FileNameUtil.renameMain(null, "a.pdf"));
+        Assertions.assertEquals("a.pdf", FileNameUtil.renameMain("", "a.pdf"));
+    }
+}
