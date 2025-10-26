a45cc8d2901f80f4d18116d6a6069fcc5ebf9e1c
Toint <599818663@qq.com>
2025-10-26 11:25:05 +0800

    fix

--- oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/bdocr/model/BaseOcrRequest.java
+++ oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/bdocr/model/BaseOcrRequest.java
@@ -2,6 +2,7 @@ package cn.toint.oktool.spring.boot.bdocr.model;
 
 import cn.hutool.v7.core.codec.binary.Base64;
 import cn.hutool.v7.core.io.IoUtil;
+import cn.hutool.v7.core.io.StreamProgress;
 import cn.hutool.v7.core.io.file.FileTypeUtil;
 import cn.hutool.v7.core.net.url.UrlEncoder;
 import cn.hutool.v7.http.HttpUtil;
@@ -9,18 +10,17 @@ import cn.hutool.v7.http.client.Request;
 import cn.hutool.v7.http.client.Response;
 import cn.hutool.v7.http.meta.HeaderName;
 import cn.hutool.v7.http.meta.HttpHeaderUtil;
-import cn.hutool.v7.http.meta.HttpStatus;
 import cn.toint.oktool.util.Assert;
 import cn.toint.oktool.util.FileNameUtil;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 优先级：image > url > pdf_file > ofd_file ，当image字段存在时，url、pdf_file、ofd_file 字段失效
@@ -94,24 +94,16 @@ public class BaseOcrRequest {
     public BaseOcrRequest file(String fileUrl) {
         Assert.notBlank(fileUrl, "fileUrl must not be blank");
 
-        // 最大重定向次数, 避免一直重定向
-        AtomicInteger maxRedirectSize = new AtomicInteger(5);
         Request request = HttpUtil.createGet(fileUrl);
+        request.setMaxRedirects(5);
         // hutool会给默认的user-agent, 默认的user-agent存在一些问题, 所以这里去掉
         request.header(HeaderName.USER_AGENT, null);
 
         try (Response response = request.send()) {
-            final int code = response.getStatus();
-
-            // http框架存在重定向资源泄露问题, 所以由自己实现重定向.
-            if (HttpStatus.isRedirected(code)) {
-                Assert.isTrue(maxRedirectSize.decrementAndGet() >= 0, "文件下载达到最大重定向次数, 已拦截");
-                return file(response.header(HeaderName.LOCATION.getValue()));
-            }
-
             // 校验头信息
+            // http信息可能伪造, 所以只校验最大值
             long contentLength = response.contentLength();
-            checkFileSize(contentLength);
+            if (contentLength > 0) checkFileSize(contentLength);
 
             // 尝试通过响应头拿到文件类型
             String extName = null;
@@ -155,7 +147,7 @@ public class BaseOcrRequest {
 
     /**
      * @param fileBytes 文件字节, 会校验字节大小
-     * @param extName 文件类型(后缀), 传空值则通过二进制头获取文件类型
+     * @param extName   文件类型(后缀), 传空值则通过二进制头获取文件类型
      */
     public BaseOcrRequest file(byte[] fileBytes, String extName) {
         Assert.notNull(fileBytes, "fileBytes must not be null");
@@ -186,7 +178,7 @@ public class BaseOcrRequest {
 
     private void checkFileSize(long fileSize) {
         if (fileSize <= 0) {
-            throw new RuntimeException("无效的文件");
+            throw new RuntimeException("空文件");
         }
 
         if (fileSize > 4 * 1024 * 1024) {
