65b829499cb7918964ef938c3316dc7a8e0c47e8
Toint <599818663@qq.com>
2025-10-26 11:34:00 +0800

    fix

--- oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/bdocr/model/BaseOcrRequest.java
+++ oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/bdocr/model/BaseOcrRequest.java
@@ -100,6 +100,10 @@ public class BaseOcrRequest {
         request.header(HeaderName.USER_AGENT, null);
 
         try (Response response = request.send()) {
+
+            // 校验状态
+            Assert.isTrue(response.isOk(), "文件链接下载失败: {}", response.getStatus());
+
             // 校验头信息
             // http信息可能伪造, 所以只校验最大值
             long contentLength = response.contentLength();
