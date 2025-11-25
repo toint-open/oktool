8add96421f7031e0ad93e4cfaa603e1ab68f2782
Toint <599818663@qq.com>
2025-11-26 02:26:47 +0800

    Remove unused imports in FlexTenantInterceptor

--- oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/flextenant/FlexTenantInterceptor.java
+++ oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/flextenant/FlexTenantInterceptor.java
@@ -16,15 +16,7 @@
 
 package cn.toint.oktool.spring.boot.flextenant;
 
-import cn.dev33.satoken.stp.StpUtil;
-import cn.toint.oktool.spring.boot.context.OkContext;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import java.util.List;
 
 /**
  * mybatis-flex租户拦截器
