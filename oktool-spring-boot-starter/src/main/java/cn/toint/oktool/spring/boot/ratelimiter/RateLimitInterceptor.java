fb0d982379cbd83eec991c1f18a3fb964acfdb2e
Toint <599818663@qq.com>
2025-11-27 18:21:59 +0800

    准备添加接口限流机制

---
+++ oktool-spring-boot-starter/src/main/java/cn/toint/oktool/spring/boot/ratelimiter/RateLimitInterceptor.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2025 Toint (599818663@qq.com)
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.toint.oktool.spring.boot.ratelimiter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 限流拦截器
+ *
+ * @author Toint
+ * @since 2025/11/27
+ */
+public class RateLimitInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
